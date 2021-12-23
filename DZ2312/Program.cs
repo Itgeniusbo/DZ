699cf814dad43f9a64d1b7a13b960042c4b4ec1c
Evgeniy <96245520+Itgeniusbo@users.noreply.github.com>
2021-12-23 22:42:51 +0300

    Добавьте файлы проекта.

---
+++ DZ2312/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace DZ2312
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var undefined = 0;
+            var mondey = 1;
+            var thuesday = 2;
+            var wednesday = 3;
+            var tuesday = 4;
+            var friday = 5;
+            var saturday = 6;
+            var sunday = 7;
+
+            Console.WriteLine("Введите номер дня недели:");
+            var number = int.Parse(Console.ReadLine() ?? "0");
+
+            if (number == undefined)
+            {
+                Console.WriteLine("Введены некорректные данные");
+            }
+            else if (number == mondey)
+            {
+                Console.WriteLine("Понедельник");
+            }
+            else if (number == thuesday)
+            {
+                Console.WriteLine("Вторник");
+            }
+            else if (number == wednesday)
+            {
+                Console.WriteLine("Среда");
+            }
+            else if (number == tuesday)
+            {
+                Console.WriteLine("tuesday");
+            }
+            else if (number == friday)
+            {
+                Console.WriteLine("Пятница");
+            }
+            else if (number == saturday)
+            {
+                Console.WriteLine("Cуббота");
+            }
+            else if (number == sunday)
+            {
+                Console.WriteLine("Воскресенье");
+            }
+        }
+    }
+}
+
+
+
+
+       
+        
